6006b97c17d02df806766d2b7f58555eb8d244b0
AlexChequer <alexac2@al.insper.edu.br>
2026-03-18 19:34:03 -0300

    auth up to date

--- src/main/java/store/auth/AuthController.java
+++ src/main/java/store/auth/AuthController.java
@@ -1,8 +1,8 @@
 package store.auth;
 
+
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,25 +10,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 import store.account.AccountOut;
 
 @FeignClient(
-    name = "auth", 
-    url = "http://auth:8080"
+    name="auth",
+    url="http://auth:8080"
 )
 public interface AuthController {
-    
+
+    public static String AUTH_COOKIE_TOKEN = "__store_jwt_token";
+
     @PostMapping("/auth/login")
-    public ResponseEntity<TokenOut> login(
+    public ResponseEntity<Void> login(
         @RequestBody LoginIn in
     );
 
-    @DeleteMapping("/auth/register")
+    @PostMapping("/auth/register")
     public ResponseEntity<Void> register(
         @RequestBody RegisterIn in
     );
 
-    @GetMapping("/auth/whoami") 
-    public ResponseEntity<AccountOut> whoami();
+    @GetMapping("/auth/whoiam")
+    public ResponseEntity<AccountOut> whoIAm();
 
-    @GetMapping("/accounts/health-check")
+    @GetMapping("/auth/health-check")
     public ResponseEntity<Void> healthCheck();
 
 }
